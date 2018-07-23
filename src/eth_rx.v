871d83111288be02c6547e3e345f9cc8315e66cf
Chase Preuninger <chasepreuninger@gmail.com>
2018-07-23 06:02:57 -0400

    Added pipeline stage for phy inputs, fixed issue with FSM encoding

--- src/eth_rx.v
+++ src/eth_rx.v
@@ -18,8 +18,18 @@ module eth_rx
 );
 	
 	`include "util.vh"
-
-	reg [63:0] data_buffer, next_data_buffer;
+	
+	reg       eth_crsdv_b;
+	reg [1:0] eth_rxd_b;
+	reg       eth_rxerr_b;
+	always @(posedge clk_mac) begin
+		eth_crsdv_b <= eth_crsdv;
+		eth_rxd_b   <= eth_rxd;
+		eth_rxerr_b <= eth_rxerr;
+	end
+	
+	reg [63:0] data_buffer;
+	reg [63:0] next_data_buffer;
 	always @(posedge clk_mac) begin
 		if(rst_n)
 			data_buffer <= next_data_buffer;
@@ -27,6 +37,7 @@ module eth_rx
 			data_buffer <= 0;
 	end
 	
+
 	reg [10:0] frame_idx, next_frame_idx;
 	reg [1:0]  dibit_cnt, next_dibit_cnt;
 	always @(posedge clk_mac) begin
@@ -49,11 +60,13 @@ module eth_rx
 		rx_err <= next_rx_err;
 	end
 		
-	localparam STATE_PREAMBLE = 0;
-	localparam STATE_SOF      = 1;
-	localparam STATE_RECV     = 2;
+	localparam STATE_PREAMBLE = 3'b001;
+	localparam STATE_SOF      = 3'b010;
+	localparam STATE_RECV     = 3'b100;
 	
-	reg [2:0] state, next_state;
+	//Override FSM because Vivado ignores STATE_RECV with auto
+	(* fsm_encoding = "none" *)
+	reg [2:0] state = STATE_PREAMBLE, next_state;
 	always @(posedge clk_mac) begin
 		if(rst_n)
 			state <= next_state; 
@@ -72,19 +85,19 @@ module eth_rx
 	);
 	
 	always @* begin
-		next_state  = state;
-		next_rx_vld = 0;
-		next_rx_eof = 0;
-		next_rx_sof = 0;
-		next_rx_err = 0; 
-		next_rx_len = rx_len;
-		next_rx_dat = rx_dat;
-		
+		next_state     = state;
+		next_rx_vld    = 0;
+		next_rx_eof    = 0;
+		next_rx_sof    = 0;
+		next_rx_err    = 0; 
+		next_rx_len    = rx_len;
+		next_rx_dat    = rx_dat;
 		next_frame_idx = frame_idx;
-		next_dibit_cnt = dibit_cnt + 1;
 		
-		if(eth_crsdv && !eth_rxerr) 
-			next_data_buffer = {eth_rxd, data_buffer[63:2]};
+		next_dibit_cnt   = dibit_cnt + 1;
+		
+		if(eth_crsdv_b && !eth_rxerr_b)
+			next_data_buffer = {eth_rxd_b, data_buffer[63:2]};
 		else
 			next_data_buffer = 0;
 		
@@ -92,16 +105,15 @@ module eth_rx
 			STATE_PREAMBLE: begin
 				next_dibit_cnt = 0;
 				next_frame_idx = 0;
-				if(data_buffer == 64'hd555555555555555) begin
-					next_state  = STATE_SOF;
-				end
+				if(data_buffer == 64'hd555555555555555)
+					next_state = STATE_SOF;
 			end STATE_RECV, STATE_SOF: begin
-				if(eth_rxerr) begin
+				if(eth_rxerr_b) begin
 					next_rx_vld = 1;
 					next_rx_eof = 1;
 					next_rx_vld = 1;
 					next_state  = STATE_PREAMBLE; 
-				end else if(eth_crsdv) begin
+				end else if(eth_crsdv_b) begin
 					if(&next_dibit_cnt) begin
 						next_frame_idx = frame_idx + 1;
 						if(frame_idx >= 4) begin
